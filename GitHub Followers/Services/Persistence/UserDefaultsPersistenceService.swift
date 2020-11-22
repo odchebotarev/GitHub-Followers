1a8f44b27b3650f049f96da3628b3e3e843fdf84
odchebotarev <33865009+odchebotarev@users.noreply.github.com>
2020-11-23 02:08:54 +0300

    Services refactored

--- GitHub Followers/Services/Persistence/UserDefaultsPersistenceService.swift
+++ GitHub Followers/Services/Persistence/UserDefaultsPersistenceService.swift
@@ -7,19 +7,6 @@
 
 import Foundation
 
-protocol PersistenceService {
-    
-    func updateWith(favorite: Follower,
-                    actionType: PersistenceActionType,
-                    completion: @escaping (GFError?) -> Void)
-    func retrieveFavorites(completion: @escaping (Result<[Follower], GFError>) -> Void)
-    func isFavorite(follower: Follower, completion: @escaping (Result<Bool, GFError>) -> Void)
-}
-
-enum PersistenceActionType {
-    case add, remove
-}
-
 struct UserDefaultsPersistenceService: PersistenceService {
     
     // MARK: - Public Methods
